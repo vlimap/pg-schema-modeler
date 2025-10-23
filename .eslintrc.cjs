c3e3e4f7ba0e3b9112a526332c00f1a8a0f075f7
Val Lima <valtemir.procopio@gmail.com>
2025-10-23 20:14:01 -0300

    chore: inicia projeto base com configs de build, lint, ts, vite e estrutura geral monorepo

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier'
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        'newlines-between': 'always'
+      }
+    ]
+  }
+};
---
+++ PROJECT_STRUCTURE.md
@@ -0,0 +1,20 @@
+# Estrutura do repositório pg-schema-modeler
+
+- apps/
+  - api/
+  - web/
+- packages/
+  - shared/
+- docker/
+  - dev/
+- .github/
+  - workflows/
+- .eslintrc.cjs
+- .prettierrc
+- vite.config.ts
+- tsconfig.json
+- jest.config.ts
+- README.md
+- Makefile
+- package.json
+- pnpm-lock.yaml
